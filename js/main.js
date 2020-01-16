ed8e3490aae960cd9148180d5739a8e7c6f83a79
matemolnar88 <matemolnar88@gmail.com>
2020-01-16 16:17:57 +0100

    Repositioning the swtich button of music

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ if(document.readyState === 'loading'){
 function ready(){
   let overlays = Array.from(document.getElementsByClassName('overlay-text'));
   let cards = Array.from(document.getElementsByClassName('card'));
-  let game = new MixOrMatch(600, cards);
+  let game = new MixOrMatch(60, cards);
 
   overlays.forEach(overlay => {
     overlay.addEventListener('click', () => {
